28f3c2380e62414615d8dbaa9865747723d3177a
Facundo Matias Caino <facundocaino@gmail.com>
2022-05-02 19:21:41 -0300

    prueba de menu js

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,8 @@
-window.onscroll = function () {
-  myFunction();
-};
+document.getElementById("btn_menu").addEventListener("click, mostrar_menu");
+menu = document.getElementbyid("header");
+nav = document.getElementbyid("nav");
 
-function myFunction() {
-  if (document.documentElement.scrollTop > 5) {
-    document.getElementById("header").className = "nav_mod_1";
-  } else {
-    document.getElementById("header").className = "";
-  }
+function mostrar_menu() {
+  menu.classList.toggle("move_conten");
+  nav.classList.toggle("move_nav");
 }
