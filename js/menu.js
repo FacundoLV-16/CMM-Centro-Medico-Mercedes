bd74a2ef497ce7c2a650a0e6a76eaa4a121cf317
Facundo Matias Caino <facundocaino@gmail.com>
2022-05-02 11:37:26 -0300

    js v1

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,7 @@ window.onscroll = function () {
 };
 
 function myFunction() {
-  if (document.documentElement.scrollTop > 50) {
+  if (document.documentElement.scrollTop > 5) {
     document.getElementById("header").className = "nav_mod_1";
   } else {
     document.getElementById("header").className = "nav_mod_2";
