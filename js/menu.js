132b95d9dc19611c57123c57a2f38231793f3dca
Facundo Matias Caino <facundocaino@gmail.com>
2022-05-03 10:32:51 -0300

    menu pc-tablet-movil responsive, pero js de scroll y menu movil no funcionan

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,2 @@
-document.getElementById("btn_menu").addEventListener("click, mostrar_menu");
-menu = document.getElementbyid("header");
-nav = document.getElementbyid("nav");
-
-function mostrar_menu() {
-  menu.classList.toggle("move_conten");
-  nav.classList.toggle("move_nav");
-}
+/** btn menu, no logro aplicar el js **/
+/** menu scroll down , lo logro aplicar el js **/
